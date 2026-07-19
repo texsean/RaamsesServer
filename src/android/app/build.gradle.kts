b7826ac5cff7a05dd3618aed25a338c9a93debde
texsean <seantexan@gmail.com>
2026-07-18 21:30:29 -0500

    chore(android): Bump version to 1.0.0 (versionCode 2)

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.raamses.console"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0-beta"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
